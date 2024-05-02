06d2b277437b76778b1a9fd697daed3ba5e15e55
ReReniao <cou-aqua@qq.com>
2024-05-02 17:11:48 +0800

    v2

--- cmd/grpc/main.go
+++ cmd/grpc/main.go
@@ -4,38 +4,46 @@ import (
 	"flag"
 	"fmt"
 	"recache/config"
-	etcdservice "recache/internal/middleware/etcd"
-	grpcserver "recache/internal/server/grpc"
+	conf "recache/config"
+	"recache/internal/middleware/etcd/discovery/discovery3"
+	"recache/internal/pkg/student/dao"
 	"recache/internal/service"
+	"recache/utils/logger"
 )
 
 var (
-	port        = flag.Int("port", 9999, "service node port")
-	serviceName = "recachepb"
+	port = flag.Int("port", 9999, "service node port")
 )
 
 func main() {
-	config.InitConfig()
+	conf.InitConfig()
+	dao.InitDB()
 	flag.Parse()
+
 	// grpc node local service address
-	// grpc 结点本地服务地址
 	serviceAddr := fmt.Sprintf("localhost:%d", *port)
 	gm := service.NewGroupManager([]string{"scores", "website"}, serviceAddr)
 
-	// get a grpc service instance
-	// 获得一个 gprc 服务实例
+	// get a grpc service instance（通过通信来共享内存而不是通过共享内存来通信）
 	updateChan := make(chan bool)
-	svr, err := grpcserver.NewServer(updateChan, serviceAddr)
+	svr, err := service.NewServer(updateChan, serviceAddr)
 	if err != nil {
-		fmt.Printf("acquire grpc server instance failed, %v\n", err)
+		logger.LogrusObj.Errorf("acquire grpc server instance failed, %v", err)
 		return
 	}
 
-	go etcdservice.DynamicServices(updateChan, serviceName)
+	go discovery3.DynamicServices(updateChan, config.Conf.Services["recache"].Name)
 
-	// Server implemented Pick interface, register a node selector for recachepb
-	svr.UpdatePeers(etcdservice.ListServicePeers(serviceName))
+	// Server implemented Pick interface, register a node selector for ggcache
+	peers, err := discovery3.ListServicePeers(config.Conf.Services["recache"].Name)
+	if err != nil {
+		peers = []string{"serviceAddr"}
+	}
+
+	svr.SetPeers(peers)
+
 	gm["scores"].RegisterServer(svr)
+
 	// start grpc service
 	svr.Start()
 }
